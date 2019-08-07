a7ba1c98582254ba306562c7b622921e480356ff
ckallum <ckallumke@gmail.com>
2019-08-07 15:34:55 +0100

    Continue

--- gameOfLife.py
+++ gameOfLife.py
@@ -1,61 +1,85 @@
 from random import randint
+import pygame
 
 
-def evolve(board):
-    newBoard = initialise()
+class GameOfLife(object):
+    def __init__(self):
+        self.width = 1000
+        self.height = 1000
+        self.board = self.initialise()
+        self.cellsize = self.width / 100
+        self.cells = 100
+        self.alivecells = self.getAliveCells()
+        self.clock = pygame.time.Clock()
 
-    for row, val in enumerate(board):
-        for col, val2 in enumerate(val):
-            count = getNeighbours((col, row), board)
-            if board[row][col]:
-                if count > 3 or count < 2:
-                    newBoard[row][col] = False
-                else:
-                    newBoard[row][col] = True
-            else:
-                if count == 3:
-                    newBoard[row][col] = True
-    return newBoard
+    def initialise(self, alive=[]):
+        board = [[False for _ in range(self.cells)] for _ in range(self.cells)]
+        if alive:
+            for col, row in alive:
+                try:
+                    self.board[row][col] = True
+                except:
+                    pass
+        return board
 
-
-def initialise(alive=[]):
-    board = [[False for _ in range(50)] for _ in range(50)]
-    if alive:
-        for col, row in alive:
+    def getNeighbours(self, cell):
+        x, y = cell
+        count = 0
+        neighbours = [(x - 1, y - 1), (x - 1, y), (x, y - 1), (x + 1, y), (x + 1, y + 1), (x + 1, y - 1),
+                      (x - 1, y + 1),
+                      (x, y + 1)]
+        for neighbour in neighbours:
+            col, row = neighbour
             try:
-                board[row][col] = True
+                if self.board[row][col]:
+                    count += 1
             except:
                 pass
-    return board
+        return count
 
+    def evolve(self):
+        new = self.initialise()
 
-def getAliveCoords(board):
-    alive = [(x, y) for x in range(len(board[0])) for y in range(len(board)) if board[y][x]]
-    return alive
+        for row, val in enumerate(self.board):
+            for col, val2 in enumerate(val):
+                count = self.getNeighbours((col, row))
+                if self.board[row][col]:
+                    if count > 3 or count < 2:
+                        new[row][col] = False
+                    else:
+                        new[row][col] = True
+                else:
+                    if count == 3:
+                        new[row][col] = True
+        self.board = new
 
+    def getAliveCells(self):
+        alive = [(x, y) for x in range(len(self.board[0])) for y in range(len(self.board)) if self.board[y][x]]
+        return alive
 
-def getNeighbours(coordinate, board):
-    x, y = coordinate
-    count = 0
-    neighbours = [(x - 1, y - 1), (x - 1, y), (x, y - 1), (x + 1, y), (x + 1, y + 1), (x + 1, y - 1), (x - 1, y + 1),
-                  (x, y + 1)]
-    for neighbour in neighbours:
-        col, row = neighbour
-        try:
-            if board[row][col]:
-                count += 1
-        except:
-            pass
-    if board[y][x]:
-        print(count, (x, y))
-    return count
+    def draw(self):
+        pass
+
+    def run(self):
+        run = True
+        pygame.init()
+        pygame.display.set_caption("Conway's Game of Life")
+
+        while run:
+            self.draw()
+            self.clock.tick()
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    run = False
+                    pygame.display.quit()
+                elif event.type == pygame.K_SPACE:
+                    self.board = self.initialise()
+        pygame.quit()
 
 
 def main():
-    board = initialise([(0, 10), (0, 1), (1, 0), (1, 1), (4, 4)])
-    for _ in range(1000):
-        print(getAliveCoords(board))
-        board = evolve(board)
+    app = GameOfLife()
+    app.run()
 
 
 if __name__ == '__main__':
